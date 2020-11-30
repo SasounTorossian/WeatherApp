5dd4cefa10405238d59e82522b62771bc573bb5c
Sasoun Torossian <sasountorossian@gmail.com>
2020-11-30 11:48:50 +0000

    Fixed alignment of header and input/submit field.

--- dist/main.js
+++ dist/main.js
@@ -1,1 +1,17 @@
-(()=>{let e=document.querySelector(".image");(async()=>{const a=await fetch("http://api.openweathermap.org/data/2.5/weather?q=London&appid=4afc0e7848e13a7ccc62b3be90d8b38f"),t=await a.json(),o=(t.main.temp-273.15).toFixed(2),n=t.main.humidity,c=t.weather[0].description,i=t.weather[0].icon,p=(await fetch("http://openweathermap.org/img/wn/"+i+"@2x.png")).url;e.src=p,console.log(o),console.log(n),console.log(c)})()})();
+/*
+ * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
+ * This devtool is not neither made for production nor for readable output files.
+ * It uses "eval()" calls to create a separate source file in the browser devtools.
+ * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
+ * or disable the default devtool with "devtool: false".
+ * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
+ */
+/******/ (() => { // webpackBootstrap
+/*!**********************!*\
+  !*** ./src/index.js ***!
+  \**********************/
+/*! unknown exports (runtime-defined) */
+/*! runtime requirements:  */
+eval("let image = document.querySelector(\".image\")\r\n\r\n// Conversion between F and C button.\r\n\r\nconst getWeather = (async () => {\r\n    const response = await fetch('http://api.openweathermap.org/data/2.5/weather?q=London&appid=4afc0e7848e13a7ccc62b3be90d8b38f')\r\n    const responseJson = await response.json()\r\n\r\n    const weatherTempKelvin = responseJson.main.temp\r\n    const weatherTempCelsius = (weatherTempKelvin - 273.15).toFixed(2)\r\n    const weatherHumid = responseJson.main.humidity\r\n    const weatherDesc = responseJson.weather[0].description\r\n    const weathericon = responseJson.weather[0].icon\r\n    const iconPng = await fetch(\"http://openweathermap.org/img/wn/\" + weathericon + \"@2x.png\")\r\n    const iconPngUrl = iconPng.url\r\n\r\n    image.src = iconPngUrl\r\n\r\n    console.log(weatherTempCelsius)\r\n    console.log(weatherHumid)\r\n    console.log(weatherDesc)\r\n})()\n\n//# sourceURL=webpack://y/./src/index.js?");
+/******/ })()
+;
